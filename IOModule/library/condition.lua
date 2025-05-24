bc4da7de4c5d116cce2d8e66f7f2b2bfbca32dd1
Ben Hagle <ben.hagle@meowwolf.com>
2025-05-24 15:29:32 -0500

    Add negate to condition description handler

--- IOModule/library/condition.lua
+++ IOModule/library/condition.lua
@@ -1,7 +1,7 @@
 ---@meta
 ---@class condition
 ---@field handler fun(properties:table<string,any>, variables:table<integer,Variant>): boolean #Condition handler function to determine if the condition is met
----@field description_handler fun(properties:table<string,any>): string #Description of the condition
+---@field description_handler fun(properties:table<string,any>, negate:boolean): string #Description of the condition
 ---@field issues_handler fun(properties:table<string,any>): string|string[]|nil #Displays issue strings based on the properties. If nil, no issues are displayed
 local condition = {}
 
